47317f32c7e0769f52880b2a989f40a3ac17fa28
nizhja <nzj19910102@163.com>
2020-03-14 10:14:03 +0800

    0121_Week01(JS)

---
+++ Week_01/G20200447010121/LeetCode_26_0121.js
@@ -0,0 +1,13 @@
+// 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+// 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+var removeDuplicates = function(nums) {
+    var i = 0
+    for(var j = 1; j < nums.length; j++) {
+        if(nums[i] != nums[j]) {
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1
+};
