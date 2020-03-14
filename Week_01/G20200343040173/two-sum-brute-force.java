bcba48898908a58457f8ab0528c3fd5d0af94122
chenyunli <m18301662790@gmail.com>
2020-03-14 16:15:51 +0800

    commit hash solution

--- Week_01/G20200343040173/two-sum-brute-force.java
+++ Week_01/G20200343040173/two-sum-brute-force.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target){
-        int[] result = new int[2];
+        int[] results = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
+                    results[0] = i;
+                    results[1] = j;
+                    break;
                 }
             }
         }
-        return result;
+        return results;
     }
 }
