1d725a6a1b341f3a62aefa55cb4008e2bd9d53d8
chenyunli <m18301662790@gmail.com>
2020-03-14 16:44:08 +0800

    commit loop twice solution

---
+++ Week_01/G20200343040173/move-zeroes-loop-twice.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moverZeros(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
