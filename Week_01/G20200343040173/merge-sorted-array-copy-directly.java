7c3478b9cd84971b1a3d0ffa6c25dc0389c8972e
chenyunli <m18301662790@gmail.com>
2020-03-14 18:31:15 +0800

    commit array copy directly solution

--- Week_01/G20200343040173/merge-sorted-array-copy-directly.java
+++ Week_01/G20200343040173/merge-sorted-array-copy-directly.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 class Solution {
-    public void merge(int[] nums1, int m, int nums2, int n) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
         System.arraycopy(nums2, 0, nums1, m, n);
         Arrays.sort(nums1);
     }
