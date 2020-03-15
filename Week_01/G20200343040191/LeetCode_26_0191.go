6fa35509f7726da67f7ae89d45243523658717a5
Yangchulei <yangchulei@megvii.com>
2020-03-15 15:56:37 +0800

    week_01 homework

---
+++ Week_01/G20200343040191/LeetCode_26_0191.go
@@ -0,0 +1,13 @@
+func removeDuplicates(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    i := 0
+    for j := 1; j < len(nums); j++ {
+        if nums[i] != nums[j] {
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1
+}
