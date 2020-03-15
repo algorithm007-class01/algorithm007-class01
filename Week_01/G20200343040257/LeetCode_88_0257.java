72d6c8a7309a9fe801a3590d80dc258b85354817
cxl123156 <chen9294@hotmail.com>
2020-03-15 13:23:27 +0800

    cxl:提交本周作业

---
+++ Week_01/G20200343040257/LeetCode_88_0257.java
@@ -0,0 +1,23 @@
+package cxl.study.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 合并两个有序数组
+ * https://leetcode-cn.com/problems/merge-sorted-array/
+ */
+public class LeetCode_88_MergeSortedArray {
+
+    /**
+     * 先append到数组后面再对整个数组进行排序
+     *
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i : nums2) {
+            nums1[m] = i;
+            m++;
+        }
+        Arrays.sort(nums1);
+    }
+
+}
