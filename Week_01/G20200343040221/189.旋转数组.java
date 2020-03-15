b5c2d393ffa0aa15393740eecfcffa3eab43db3c
王利东 <370034128@qq.com>
2020-03-15 21:38:56 +0800

    第一周作业

---
+++ Week_01/G20200343040221/189.旋转数组.java
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=java
+ *
+ * [189] 旋转数组
+ *
+ * https://leetcode-cn.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (40.73%)
+ * Likes:    513
+ * Dislikes: 0
+ * Total Accepted:    106.1K
+ * Total Submissions: 260.3K
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [-1,-100,3,99] 和 k = 2
+ * 输出: [3,99,-1,-100]
+ * 解释: 
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ * 
+ * 说明:
+ * 
+ * 
+ * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+ * 要求使用空间复杂度为 O(1) 的 原地 算法。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+
+    public void rotate1(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            int temp = nums[nums.length - 1];
+            for (int j = nums.length - 2; j >= 0; j--) {
+                nums[j + 1] = nums[j];
+            }
+            nums[0] = temp;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        if (nums.length == 1) {
+            return;
+        }
+
+        k = k % nums.length;
+
+        int[] temps = new int[k];
+        for (int i = 0; i < k; i++) {
+            temps[i] = nums[nums.length - k + i];
+        }
+        for (int j = nums.length - k - 1; j >= 0; j--) {
+            nums[j + k] = nums[j];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temps[i];
+        }
+    }
+
+}
+// @lc code=end
+
