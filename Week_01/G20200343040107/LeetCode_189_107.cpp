416b55861c4f40eef3f996121e515e0899ff72b7
gaotianpeng <gtpsydm@gmail.com>
2020-03-16 09:15:39 +0800

    0107_Week02(c++)

--- Week_01/G20200343040107/LeetCode_189_107.cpp
+++ Week_01/G20200343040107/LeetCode_189_107.cpp
@@ -4,38 +4,7 @@ using namespace  std;
 
 
 /*
-    remove-duplicates-from-sorted-array/
-*/
-int removeDuplicates(vector<int>& nums) {
-    if(nums.size() <= 0) {
-        return 0; 
-    }
-
-    int len = nums.size(); 
-    int updateIndex = 0; 
-    for(int j = 1; j < len; j++) {
-        if(nums[updateIndex] != nums[j]) {
-            updateIndex++; 
-            nums[updateIndex] = nums[j];
-        }; 
-    }
-    return updateIndex+1;
-}
-
-void test_remove_duplicate() {
-    vector<int> empty_vec; 
-    cout <<  "empty array test: " << removeDuplicates(empty_vec) << endl;
-
-    vector<int> no_dup_vec = {1, 2, 3, 4, 5};
-    cout <<  "no dup array test: " << removeDuplicates(no_dup_vec) << endl;
-
-    vector<int> dup_vec = {1, 2, 2, 3, 4, 4, 4, 5, 5, 5};
-    cout <<  "no dup array test: " << removeDuplicates(dup_vec) << endl;
-}
-
-
-/*
-    rotate-array
+    189: rotate-array
 */
 void swap(int &x, int &y) {
     x = x ^ y;
@@ -82,7 +51,6 @@ void rotate_test() {
 
 
 int main(int argc, char* argv[]) {
-    test_remove_duplicate();
     rotate_test();
     return 0; 
 }
