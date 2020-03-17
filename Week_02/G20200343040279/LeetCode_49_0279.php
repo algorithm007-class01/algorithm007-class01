4ef6e29b5b723c3fe33aba0a47dcd1a0ad2734ae
xubaoguo <xubaoguo196@11.com>
2020-03-17 09:43:44 +0800

    第二周作业

---
+++ Week_02/G20200343040279/LeetCode_49_0279.php
@@ -0,0 +1,19 @@
+<?php
+class Solution {
+
+    /**
+     * 分组异位词
+     * @param String[] $strs
+     * @return String[][]
+     */
+    function groupAnagrams($strs) {
+    	$map = [];
+    	foreach ($strs as $str) {
+    		$arr = str_split($str);
+    		sort($arr);
+    		$tmp_str = implode("", $arr);
+    		$map[$tmp_str][] = $str;
+    	}
+    	return array_values($map);
+    }
+}
