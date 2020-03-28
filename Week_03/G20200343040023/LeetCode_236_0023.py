35f87144f69173fdcc2d7ce483197c4a6ca8d2eb
wangxc <wangxiaochuan@licaigc.com>
2020-03-29 01:52:05 +0800

    0023_Week03(py)

---
+++ Week_03/G20200343040023/LeetCode_236_0023.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+
+    # 递归
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        if not root or root.val == p.val or root.val == q.val:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left if left else right
