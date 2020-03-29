fc4b67f15e7689b75070a5e6107361a67c6d3484
salmonspirit <cch_cuncaoxin@sina.com>
2020-03-29 23:30:01 +0800

    0095_Week03

---
+++ Week_03/G20200343040095/Solution-589.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=589 lang=java
+ *
+ * [589] N叉树的前序遍历
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            res.add(current.val);
+            for (int i = current.children.size() - 1; i >= 0; i--) {
+                stack.push(current.children.get(i));
+            }
+        }
+        return res;
+    }
+
+}
+// @lc code=end
+
