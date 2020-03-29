d998e8121dcb4cfad5450985d0f6c07e64846be7
caopengfei <pengfei.cao@360che.com>
2020-03-29 22:03:32 +0800

    0003_Week03(php)

---
+++ Week_03/G20200343040003/LeetCode_144_0003.php
@@ -0,0 +1,21 @@
+<?php
+
+class Solution {
+    
+    /**
+     * @param TreeNode $root
+     * @return Integer[]
+     */
+    function preorderTraversal($root) {
+        $res = [];
+        $this->helper($root, $res);
+        return $res;
+    }
+    
+    function helper($root, &$res) {
+        if ($root == null) return ;
+        $res[] = $root->val;
+        $this->helper($root->left, $res);
+        $this->helper($root->right, $res);
+    }
+}
