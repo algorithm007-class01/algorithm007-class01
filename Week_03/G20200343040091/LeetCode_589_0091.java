1dec40976e9c74661c19b12e22db4b83150f95ba
SteelHuaSheng <rrq666@126.com>
2020-03-29 22:32:02 +0800

    0091_Week03（java）
    0091_Week03（java）

---
+++ Week_03/G20200343040091/LeetCode_589_0091.java
@@ -0,0 +1,26 @@
+ class Solution {
+
+    /**
+     * 589. N叉树的前序遍历
+     * <p>
+     * 【递归求解】
+     **/
+
+    List<Integer> res = new ArrayList<Integer>();
+
+    public List<Integer> preorder(Node root) {
+      helper(root);
+      return res;
+    }
+
+    public void helper(Node root) {
+      if (root == null) {
+        return;
+      }
+      res.add(root.val);
+      for (int i = 0; i < root.children.size(); i++) {
+        helper(root.children.get(i));
+      }
+    }
+
+  }
