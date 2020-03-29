628752e8365e67fccb418ffacbc69db8f2fbc4b5
zhangran <zhangran@42.com>
2020-03-30 03:10:34 +0800

    week03_0313

---
+++ Week_03/G20200343040313/LeetCode_429_0313.java
@@ -0,0 +1,96 @@
+package leetcode.editor.cn;//给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。
+//
+// 例如，给定一个 3叉树 : 
+//
+// 
+//
+// 
+//
+// 
+//
+// 返回其层序遍历: 
+//
+// [
+//     [1],
+//     [3,2,4],
+//     [5,6]
+//]
+// 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 树的深度不会超过 1000。 
+// 树的节点总数不会超过 5000。 
+// Related Topics 树 广度优先搜索
+
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution429 {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        Deque<Node> queue = new ArrayDeque<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.addLast(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                Node n = queue.removeFirst();
+                level.add(n.val);
+                for (Node node : n.children) {
+                    queue.addLast(node);
+                }
+            }
+
+            result.add(level);
+        }
+
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
