e3ded5e0ea22292417cde2d6834a5dd1c96ce66d
hodiligent <39579579+hodiligent@users.noreply.github.com>
2020-03-29 22:37:32 +0800

    Add files via upload

---
+++ Week_03/G20200343040009/LeetCode94.java
@@ -0,0 +1,26 @@
+package com.studyWeek3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 中序遍历
+ */
+public class LeetCode94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+}
