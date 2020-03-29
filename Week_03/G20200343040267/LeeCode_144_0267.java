bc4f3064a3ff42e31e6de82e17f5b77438dce90c
lvzhenchang2016 <lvzhenchang2016@126.com>
2020-03-29 14:28:10 +0800

    0267_Week03（Java）
    0267_Week03（Java）

---
+++ Week_03/G20200343040267/LeeCode_144_0267.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> pre = new LinkedList<Integer>();
+        preHelper(root,pre);
+        return pre;
+    }
+    public void preHelper(TreeNode root, List<Integer> pre) {
+        if(root==null) return;
+        pre.add(root.val);
+        preHelper(root.left,pre);
+        preHelper(root.right,pre);
+    }
+}
