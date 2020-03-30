61b70390a07e0ec911c528155977d2940472cbc7
wangcheng <chengwang199616@gmail.com>
2020-03-30 21:01:40 +0800

    0241_Week03（java）

--- Week_03/G20200343040241/LeetCode_590_0241.java
+++ Week_03/G20200343040241/LeetCode_590_0241.java
@@ -1,10 +1,29 @@
-public class LeetCode_641_0241 {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LeetCode_590_0241 {
 
     /*给定一个 N 叉树，返回其节点值的后序遍历。 */
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
     /**
      * 递归方法
      */
-    class Solution {
+    class SolutionOne {
         public List<Integer> postorder(Node root) {
             List<Integer> target = new ArrayList<>();
             postOrder(root,target);
@@ -26,7 +45,7 @@ public class LeetCode_641_0241 {
      * 时间复杂度：O(M)
      * 空间复杂度：O(M) M是N叉树中的节点个数
      */
-    class Solution {
+    class SolutionTwo {
         public List<Integer> postorder(Node root) {
             LinkedList<Node> stack = new LinkedList<>();
             LinkedList<Integer> output = new LinkedList<>();
