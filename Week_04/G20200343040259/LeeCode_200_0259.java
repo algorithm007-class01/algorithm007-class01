8b597ea60f02b80cd93ade8105ad940d64f5034d
iminit <iminit@126.com>
2020-04-05 22:41:58 +0800

    update

---
+++ Week_04/G20200343040259/LeeCode_200_0259.java
@@ -0,0 +1,48 @@
+package week4;
+
+public class l200numberOfIslands {
+
+    public static void main(String[] args) {
+//        char[][] grid = {{'1', '1', '1', '1', '0'},
+//                {'1', '1', '0', '1', '0'},
+//                {'1', '1', '0', '0', '0'},
+//                {'0', '0', '0', '0', '1'}};
+        char[][] grid = {{'1', '1', '1', '1', '0'},
+                        {'1', '1', '0', '1', '0'},
+                        {'1', '1', '0', '0', '0'},
+                        {'0', '0', '0', '0', '0'}};
+        int count = new Solution().numIsLands(grid);
+        System.out.println(count);
+    }
+}
+
+class Solution {
+
+    private int m;
+    private int n;
+
+    public int numIsLands(char[][] grid) {
+        int count = 0;
+        n = grid.length;
+        if (n == 0) return 0;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+        grid[i][j] = 0;
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+    }
+}
