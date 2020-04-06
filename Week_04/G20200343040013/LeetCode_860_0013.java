7c74da10fec3c665e7664b0d9ecbf984e68801fc
张达 <zhangda1@mafengwo.com>
2020-04-06 14:53:00 +0800

    week_04

---
+++ Week_04/G20200343040013/LeetCode_860_0013.java
@@ -0,0 +1,30 @@
+/**
+ * 柠檬零钱
+ */
+public boolean lemonadeChange(int[] bills) {
+
+    int five = 0;
+    int ten = 0;
+
+    for (int bill : bills) {
+        if (bill == 5) {
+            five++;
+        } else if (bill == 10) {
+            if (five == 0) {
+                return false;
+            }
+            five--;
+            ten++;
+        } else if (bill == 20) {
+            if(five > 0 && ten > 0) {
+                five--;
+                ten--;
+            } else if (five >= 3) {
+                five -= 3;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+}
