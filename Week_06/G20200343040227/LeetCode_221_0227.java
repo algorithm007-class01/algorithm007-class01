bf016c9e3ded6f53a2931bd04f2db419e4462481
mysunnygirl <605211811@qq.com>
2020-04-19 22:44:06 +0800

    week6 homework

---
+++ Week_06/G20200343040227/LeetCode_221_0227.java
@@ -0,0 +1,79 @@
+package week6.lesson12.p4.homework;
+
+/**
+ * 221. 最大正方形
+ * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:
+ * <p>
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * <p>
+ * 输出: 4
+ */
+public class LeetCode_221_0227 {
+
+    public int maximalSquare(char[][] matrix) {
+
+        //method1 暴力法 递归
+//        return method1(matrix);
+        //动态规划
+        return method2(matrix);
+//        return method3(matrix);
+    }
+
+
+    private int method2(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int maxLen = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
+                }
+            }
+        }
+        return maxLen * maxLen;
+
+    }
+
+    public int method1(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int maxsqlen = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    int sqlen = 1;
+                    boolean flag = true;
+                    while (sqlen + i < rows && sqlen + j < cols && flag) {
+                        for (int k = j; k <= sqlen + j; k++) {
+                            if (matrix[i + sqlen][k] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        for (int k = i; k <= sqlen + i; k++) {
+                            if (matrix[k][j + sqlen] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag)
+                            sqlen++;
+                    }
+                    if (maxsqlen < sqlen) {
+                        maxsqlen = sqlen;
+                    }
+                }
+            }
+        }
+        return maxsqlen * maxsqlen;
+    }
+
+}
