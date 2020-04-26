cebc6a78fa00abdba0c351389e1d8b2e94589601
Darin_L <darin_l@me.com>
2020-04-26 23:54:40 +0800

    0169_Week07(java)

---
+++ Week_07/G20200343040169/LeetCode_70_0169.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /** 
+    *记忆化递归
+    *时间复杂度:
+    *    O(n)
+    *空间复杂度:
+    *    O(n)
+    **/
+    public int climbStairs(int n) {
+        int memo[] = new int[n + 1];
+        return climb_Stairs(0, n, memo);
+    }
+    public int climb_Stairs(int i, int n, int memo[]) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+        memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
+        return memo[i];
+    }
+}
