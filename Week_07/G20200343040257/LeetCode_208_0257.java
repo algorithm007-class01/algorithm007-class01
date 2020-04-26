5983b3d19f860b5b46aee8baf1473b1ad418bf7a
chenxl <chen9294@hotmail.com>
2020-04-26 23:04:36 +0800

    cxl：提交第七周作业

---
+++ Week_07/G20200343040257/LeetCode_208_0257.java
@@ -0,0 +1,50 @@
+package cxl.study.leetcode;
+
+public class LeetCode_208_ImplementTriePrefixTree {
+
+    class Trie {
+
+        private final int ALPHABET_SIZE = 26;
+        private Trie[] children = new Trie[ALPHABET_SIZE];
+        boolean isEndOfWord = false;
+
+        public Trie() {
+        }
+
+        public void insert(String word) {
+            Trie tmp = this;
+            for (char i : word.toCharArray()) {
+                if (tmp.children[i - 'a'] == null) {
+                    tmp.children[i - 'a'] = new Trie();
+                }
+                tmp = tmp.children[i - 'a'];
+            }
+            tmp.isEndOfWord = true;
+        }
+
+        public boolean search(String word) {
+            Trie tmp = this;
+            for (char i : word.toCharArray()) {
+                if (tmp.children[i - 'a'] == null) {
+                    return false;
+                }
+                tmp = tmp.children[i - 'a'];
+            }
+            return tmp.isEndOfWord ? true : false;
+        }
+
+        public boolean startsWith(String prefix) {
+            Trie tmp = this;
+            for (char i : prefix.toCharArray()) {
+                if (tmp.children[i - 'a'] == null) {
+                    return false;
+                }
+                tmp = tmp.children[i - 'a'];
+            }
+            return true;
+        }
+
+    }
+
+
+}
