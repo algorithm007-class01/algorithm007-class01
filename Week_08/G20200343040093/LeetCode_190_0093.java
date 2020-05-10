0adf0dad6db052e5117859399e5e692396dda3db
行僧 <xing_seng@hunliji.com>
2020-05-10 23:02:27 +0800

    第8周

---
+++ Week_08/G20200343040093/LeetCode_190_0093.java
@@ -0,0 +1,9 @@
+class LeetCode_190_0093 {
+    public int reverseBits(int n) {
+        int ans = 0;
+        for (int bitsSize = 31; n != 0; n = n >>> 1, bitsSize--) {
+            ans += (n & 1) << bitsSize;
+        }
+        return ans;
+    }
+}
