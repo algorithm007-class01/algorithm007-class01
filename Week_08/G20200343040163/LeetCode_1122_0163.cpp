dc4fc4a38b6fbb1873f718ebc3de83cf2150fda8
lutianaicai <lihaoshu2046@gmail.com>
2020-05-10 23:44:33 +0800

    0163_Week08（cpp）

--- Week_08/G20200343040163/LeetCode_1122_0163.cpp
+++ Week_08/G20200343040163/LeetCode_1122_0163.cpp
@@ -4,20 +4,16 @@ public:
         unordered_map<int, int> map;
         vector<int> res;
         vector<int> restemp;
-        for (int i = 0; i < arr1.size(); i++)
-        {
+        for (int i = 0; i < arr1.size(); i++)  {
           map[arr1[i]]++;
         }
-        for (int i = 0; i < arr2.size(); i++)
-        {
-          while (map[arr2[i]] > 0)
-          {
+        for (int i = 0; i < arr2.size(); i++) {
+          while (map[arr2[i]] > 0) {
             res.push_back(arr2[i]);
             map[arr2[i]]--;
           }
         }
-        for (int num: arr1)
-        {
+        for (int num: arr1) {
           if (map[num] > 0) {
             restemp.push_back(num);
           }
