822fa6d7b9e886559bd3a0f9389856f5ee4c2b50
wangxc <wangxiaochuan@licaigc.com>
2020-05-10 20:20:52 +0800

    0023_week 08(py)

---
+++ Week_08/G20200343040023/LeetCode_191_0023.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n:
+            n &= n - 1
+            count += 1
+        return count
