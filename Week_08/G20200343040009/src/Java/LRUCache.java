2c8e865a2543bc92d729287f988f43ab7fd6caba
LinJinhao <diligentljh@163.com>
2020-05-13 08:07:19 +0800

    fix:提交第九周作业

---
+++ Week_08/G20200343040009/src/Java/LRUCache.java
@@ -0,0 +1,28 @@
+/**
+ * LRU缓存机制
+ */
+public class LRUCache {
+
+    class DoubleLinkedNode {
+        int key;
+        int value;
+        DoubleLinkedNode prev;
+        DoubleLinkedNode next;
+    }
+
+    private void addNode(DoubleLinkedNode node) {
+
+    }
+
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
