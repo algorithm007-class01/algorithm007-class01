873bceb1956a7882091165cd3e7214ab6009f3da
salmonspirit <cch_cuncaoxin@sina.com>
2020-05-17 23:02:28 +0800

    0095_Week09(Java)

---
+++ Week_09/G20200343040095/LeetCode_8_0095.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=8 lang=java
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+
+// @lc code=start
+class Solution {
+    public int myAtoi(String str) {
+        char[] chars = str.toCharArray();
+        int n = chars.length;
+        int idx = 0;
+        while (idx < n && chars[idx] == ' ') {
+            idx++;
+        }
+        if (idx == n) {
+            return 0;
+        }
+
+        boolean negative = false;
+        if (chars[idx] == '-') {
+            negative = true;
+            idx++;
+        } else if (chars[idx] == '+') {
+            idx++;
+        } else if (!Character.isDigit(chars[idx])) {
+            return 0;
+        }
+
+        int ans = 0;
+        while (idx < n && Character.isDigit(chars[idx])) {
+            int digit = chars[idx] - '0';
+            if (ans > (Integer.MAX_VALUE - digit) / 10) {
+                return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            ans = ans * 10 + digit;
+            idx++;
+        }
+        return negative ? -ans : ans;
+    }
+}
+// @lc code=end
+
