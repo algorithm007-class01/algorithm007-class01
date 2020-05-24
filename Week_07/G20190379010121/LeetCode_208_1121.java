628a407a157a8ff7a0b3a755195cf03fa0cda677
suke <suke1@100tal.com>
2020-05-24 17:40:10 +0800

    add week07

--- Week_07/G20190379010121/LeetCode_208_1121.java
+++ Week_07/G20190379010121/LeetCode_208_1121.java
@@ -37,8 +37,3 @@ class Trie {
         return true;
     }
 }
-
-作者：Guilong
-        链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree/solution/jing-jian-javashi-xian-by-guilong/
-        来源：力扣（LeetCode）
-        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
