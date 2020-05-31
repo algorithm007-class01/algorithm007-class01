77360758ad078f1fb3d01f3018dd1e18fb0b7790
lvzhenchang2016 <lvzhenchang2016@126.com>
2020-05-31 22:17:05 +0800

    0267_Week09（Java）

---
+++ Week_09/G20200343040267/LeetCode_300_0267.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < len; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < len; ++i) {
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+}
